975254c6c83c00f61696332237d7a6198636dea6
Jouni Männistö <jouni.mannisto@gmail.com>
2023-07-13 18:39:39 +0300

    9.21: Patientor, step2

---
+++ src/components/PatientDetailsPage/index.tsx
@@ -0,0 +1,108 @@
+import React, { useEffect } from "react";
+import axios from "axios";
+import { useParams } from "react-router-dom";
+import MaleIcon from '@mui/icons-material/Male';
+import FemaleIcon from '@mui/icons-material/Female';
+
+import { Patient } from "../../types";
+
+// import EntryDetails from "./EntryDetails";
+
+
+import { apiBaseUrl } from "../../constants";
+
+ const usePatient = (id: string | undefined) => {
+        const [patient, setPatient] = React.useState<Patient | undefined>();
+        const [error, setError] = React.useState<string | undefined>();
+        const [loading, setLoading] = React.useState<boolean>(true);
+
+        useEffect(() => {
+            const fetchPatient = async () => {
+                try {
+                    const { data: patient } = await axios.get<Patient>(
+                        `${apiBaseUrl}/patients/${id}`
+                    );
+                    setPatient(patient);
+                } catch (e) {
+                    console.error(e);
+                    if(e instanceof Error) {
+                        setError(e.message);
+                    } else {
+                        setError("Unknown error");
+                    }
+
+                } finally {
+                    setLoading(false);
+                }
+            };
+            void fetchPatient();
+        }, [id]);
+
+        return { patient, error, loading };
+    };
+
+
+export const PatientDetailsPage = () : JSX.Element => {
+    const { id } = useParams<{ id: string }>();
+    const { patient, error, loading } = usePatient(id);
+
+    // uesPatient hook
+
+
+
+    // fetch patient details from api
+    useEffect(() => {
+        const fetchPatient = async () => {
+            try {
+                const { data: patient } = await axios.get<Patient>(
+                    `${apiBaseUrl}/patients/${id}`
+                );
+                // dispatch(setPatient(patient));
+            } catch (e) {
+                console.error(e);
+            }
+        };
+        void fetchPatient();
+    }, [id]);
+
+
+    if (loading) {
+        return <div>Loading...</div>;
+    }
+
+    if (error) {
+        return <div>{error}</div>;
+    }
+
+    if (!patient) {
+        return <div>Patient not found</div>;
+    }
+
+    return (
+        <div>
+            <h2>{patient.name}
+            {patient.gender === "male" ? <MaleIcon /> : <FemaleIcon />}
+            </h2>
+
+            <div>ssn: {patient.ssn}</div>
+            <div>occupation: {patient.occupation}</div>
+            <h3>entries</h3>
+            {/* {patient.entries.map((entry: { id: React.Key | null | undefined; diagnosisCodes: any[]; }) => {
+                return (
+                    <div key={entry.id}>
+                        <EntryDetails entry={entry} />
+                        <ul>
+                            {entry.diagnosisCodes?.map(code => {
+
+                                const diagnosis = diagnosis.find((d: { code: any; }) => d.code === code);
+                                return (
+                                    <li key={code}>{code} {diagnosis?.name}</li>
+                                );
+                            })}
+                        </ul>
+                    </div>
+                );
+            })} */}
+        </div>
+    );
+}
